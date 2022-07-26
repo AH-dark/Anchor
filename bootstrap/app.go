c357bf2cbe1daedcbe9dc6df9ccd61cba09e91b3
AH-dark <ahdark@outlook.com>
2022-07-26 22:25:47 +0800

    Fix: gin logger mode not set

--- bootstrap/app.go
+++ bootstrap/app.go
@@ -1,7 +1,15 @@
 package bootstrap
 
-import "github.com/AH-dark/Anchor/pkg/conf"
+import (
+	"github.com/AH-dark/Anchor/pkg/conf"
+	"github.com/gin-gonic/gin"
+)
 
 func Init(path string) {
 	conf.Init(path)
+
+	// Debug 关闭时，切换为生产模式
+	if !conf.Config.System.Debug {
+		gin.SetMode(gin.ReleaseMode)
+	}
 }
